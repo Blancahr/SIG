0cbea0ac39269e7e75f4ce6f131ed94db12868fc
Blancahr <143891402+Blancahr@users.noreply.github.com>
2026-03-25 02:58:01 +0000

    Se implementaron los módulos de usuarios, reportes y gestión de documentos utilizando Vue.js. Se integraron componentes dinámicos y se realizaron pruebas funcionales.

---
+++ src/components/Usuarios.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Gestión de Usuarios</h2>
+
+    <input v-model="nombre" placeholder="Nombre" />
+    <input v-model="correo" placeholder="Correo" />
+
+    <button @click="agregarUsuario">Agregar</button>
+
+    <ul>
+      <li v-for="user in usuarios" :key="user.id">
+        {{ user.nombre }} - {{ user.correo }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: '',
+      correo: '',
+      usuarios: []
+    }
+  },
+  methods: {
+    agregarUsuario() {
+      if (!this.nombre || !this.correo) {
+        alert("Completa los campos")
+        return
+      }
+
+      this.usuarios.push({
+        id: Date.now(),
+        nombre: this.nombre,
+        correo: this.correo
+      })
+
+      this.nombre = ''
+      this.correo = ''
+    }
+  }
+}
+</script>
